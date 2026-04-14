aff781c0be21ba3f829d16c7dbb6d965509f376e
tanaka <c0c2504419@edu.teu.ac.jp>
2026-04-14 17:02:16 +0900

    演習課題2:move_ipメソッドを一つにまとめた

--- pygame_image.py
+++ pygame_image.py
@@ -21,16 +21,17 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed() #すべてのキーの押下状態の取得
-        kk_rct.move_ip((-1, 0))
+        x1 = -1
+        y1 = 0
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0,-1))
+            y1 = -1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0,1))
+            y1 = 1
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1,0))
+            x1 = -1
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((2,0))#練習10
-        
+            x1 = 2#練習10
+        kk_rct.move_ip((x1, y1))
         x = tmr % 3200#練習5
         screen.blit(bg_img, [-x, 0]) #練習2
         screen.blit(bg_img2, [-x+1600, 0]) #練習7
